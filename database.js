a94067877628df0a12c95598efe9c897c96f05b0
Sanchit Mittal <sanchit.mitttal@hotcocoasfotware.com>
2018-11-13 00:22:15 +0530

    linked with mysql database

---
+++ database.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'cocubes'
+  });
+
+connection.connect(function(err){
+    if(!err) {
+        console.log("Database is connected ... ");    
+    } else {
+        console.log(`Error connecting database ... ${err}`);    
+    }
+});
+
+module.exports = connection;
