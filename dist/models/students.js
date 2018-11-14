5ba7be5820ee80e763a55ea7adf370252ca37098
Sanchit Mittal <sanchit.mitttal@hotcocoasfotware.com>
2018-11-14 17:01:49 +0530

    completed student model class

--- dist/models/students.js
+++ dist/models/students.js
@@ -1,10 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class Students {
-    constructor(name, email) {
+const Base_1 = require("./basemodel/Base");
+class Students extends Base_1.Base {
+    constructor(id, created, updated, deleted, name, email) {
+        super(id, created, updated, deleted);
         this.name = name;
         this.email = email;
     }
 }
 exports.Students = Students;
-//# sourceMappingURL=students.js.map
+//# sourceMappingURL=Students.js.map
--- src/models/students.ts
+++ src/models/students.ts
@@ -1,8 +1,11 @@
-export class Students {
+import { Base } from "./basemodel/Base";
+
+export class Students extends Base {
     name: String
     email: String
 
-    constructor(name: String, email: String) {
+    constructor(id: BigInteger, created: BigInteger, updated: BigInteger, deleted: BigInteger, name: String, email: String) {
+        super(id, created, updated, deleted);
         this.name = name
         this.email = email
     }
