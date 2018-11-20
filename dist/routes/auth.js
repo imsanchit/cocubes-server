1feb43ab08fd1c329b574561b8f7e01ded19b542
Sanchit Mittal <sanchit.mitttal@hotcocoasfotware.com>
2018-11-21 02:25:36 +0530

    auth flow with sending mail completed, require refactoring

--- dist/routes/auth.js
+++ dist/routes/auth.js
@@ -12,42 +12,90 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+var nodemailer = require('nodemailer');
+var jwt = require("jsonwebtoken");
 const database_1 = __importDefault(require("../config/database"));
 var auth = express_1.Router();
 auth.route('/signup')
-    .get((req, res) => {
-    (function fetchUsers() {
-        return __awaiter(this, void 0, void 0, function* () {
-            database_1.default.query('SELECT * from students', function (error, results, fields) {
-                if (error)
-                    throw error;
-                console.log(results);
-                res.send(results);
-            });
-        });
-    }());
-    // res.json(req.body);
-})
     .post((req, res) => {
     const { username, email, phoneNumber } = req.body;
-    // const student = new Students("sanchit", "sanchitmittal1@gmail.com", "password", 1, 1, "1234", "male", new Date(), "line-1", "line-2", "city", "state", "country", 1, "class");        
+    const user = {
+        username: username,
+        email: email,
+        phoneNumber: phoneNumber
+    };
     (function insertUser() {
         return __awaiter(this, void 0, void 0, function* () {
             const query = `INSERT INTO \`students\`(\`Created\`, \`Name\`, \`Email\`, \`Phone_Number\`, \`User_TypeID\`) VALUES (${Date.now()}, "${username}", "${email}", "${phoneNumber}", 1)`;
-            // console.log(`query is ${query}`);
             database_1.default.query(`${query}`, function (error, results, fields) {
-                if (error && error.code == "ER_DUP_ENTRY") {
+                if (error) {
                     const errorResp = {
                         "code": error.code,
                         "message": error.message
                     };
                     res.json(errorResp);
+                    return;
                 }
-                console.log(results);
-                res.json(results);
+                const { insertId } = results;
+                jwt.sign({
+                    userId: insertId
+                }, "secretKey", { expiresIn: '1d'
+                }, (err, token) => {
+                    var tomorrow = new Date();
+                    tomorrow.setDate((new Date()).getDate() + 1);
+                    const expiration = tomorrow.getTime();
+                    const query = `INSERT INTO \`tokens\`(\`Created\`, \`UserID\`, \`Hash\`, \`Email_verified\`, \`Expiration\`) VALUES (${Date.now()}, "${insertId}", "${token}", false, "${expiration}")`;
+                    database_1.default.query(`${query}`, function (error, results, fields) {
+                        if (error) {
+                            const errorResp = {
+                                "code": error.code,
+                                "message": error.message
+                            };
+                            res.json(errorResp);
+                            return;
+                        }
+                        res.json(results);
+                        var transporter = nodemailer.createTransport({
+                            service: 'gmail',
+                            auth: {
+                                type: 'OAuth2',
+                                user: 'sanchit13103509cse@gmail.com',
+                                clientId: '298351554194-lkm6ls22gg5seqhtq9u3htltt5aaqhp6.apps.googleusercontent.com',
+                                clientSecret: 'zKrPXnFbUFfg0guacTpp2Bop',
+                                refreshToken: '1/Wf3afmRUXsUk7zbRXNaUAln_m_9c7hn6uq4-VmTpOHZ_GjbUoK9sxj5hEipw2HG0'
+                            }
+                        });
+                        const url = `http://localhost:3007/welcome?token=${token}`;
+                        var mailOptions = {
+                            from: 'Sanchit Mittal <sanchit13103509cse@gmail.com>',
+                            to: email,
+                            subject: 'Verify email',
+                            html: `Please click this email to confirm your email: <a href="${url}">${url}</a>`
+                        };
+                        transporter.sendMail(mailOptions, function (err, res) {
+                            if (err) {
+                                console.log('Error');
+                            }
+                            else {
+                                console.log("Message sent: " + res.message);
+                            }
+                            transporter.close();
+                        });
+                    });
+                });
             });
         });
     }());
 });
 exports.default = auth;
+// .get((req: Request, res: Response) => {
+//     (async function fetchUsers(){
+//         dbConnection.query('SELECT * from students', function (error: MysqlError | null, results?: any, fields?: FieldInfo[]) {
+//             if (error) throw error;
+//             console.log(results)
+//             res.send(results)
+//         })
+//     }())
+//     // res.json(req.body);
+// })
 //# sourceMappingURL=auth.js.map
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,30 +1,27 @@
 import {Request, Response, Router} from 'express';
 import { MysqlError, FieldInfo } from 'mysql';
+var nodemailer = require('nodemailer');
+var jwt = require("jsonwebtoken");
 import dbConnection from "../config/database"
-import Students from "../models/Students";
+import { JsonWebTokenError } from "jsonwebtoken";
 
 var auth = Router();
 
 auth.route('/signup')
-    .get((req: Request, res: Response) => {
-        (async function fetchUsers(){
-            dbConnection.query('SELECT * from students', function (error: MysqlError | null, results?: any, fields?: FieldInfo[]) {
-                if (error) throw error;
-                console.log(results)
-                res.send(results)
-            })
-        }())
-        // res.json(req.body);
-    })
     .post((req: Request, res: Response) => {
+
         const { username, email, phoneNumber } = req.body;
-        // const student = new Students("sanchit", "sanchitmittal1@gmail.com", "password", 1, 1, "1234", "male", new Date(), "line-1", "line-2", "city", "state", "country", 1, "class");        
+
+        const user = {
+            username: username,
+            email: email,
+            phoneNumber: phoneNumber 
+        };
+
         (async function insertUser(){
             const query = `INSERT INTO \`students\`(\`Created\`, \`Name\`, \`Email\`, \`Phone_Number\`, \`User_TypeID\`) VALUES (${Date.now()}, "${username}", "${email}", "${phoneNumber}", 1)`;
-            // console.log(`query is ${query}`);
             dbConnection.query(`${query}`, function(error: MysqlError | null, results?: any, fields?: FieldInfo[]) {
                 if (error) {
-                    // res.json(error);
                     const errorResp = {
                         "code": error.code,
                         "message": error.message
@@ -32,9 +29,74 @@ auth.route('/signup')
                     res.json(errorResp)
                     return
                 }
-                console.log(results)
-                res.json(results)
+
+                const { insertId } = results;
+                  
+                jwt.sign({
+                    userId: insertId
+                }, "secretKey", { expiresIn: '1d'
+
+                }, (err: JsonWebTokenError, token: any) => {
+                    var tomorrow = new Date();
+                    tomorrow.setDate((new Date()).getDate() + 1);
+                    const expiration = tomorrow.getTime();
+
+                    const query = `INSERT INTO \`tokens\`(\`Created\`, \`UserID\`, \`Hash\`, \`Email_verified\`, \`Expiration\`) VALUES (${Date.now()}, "${insertId}", "${token}", false, "${expiration}")`;
+                    dbConnection.query(`${query}`, function(error: MysqlError | null, results?: any, fields?: FieldInfo[]) {
+                        if (error) {
+                            const errorResp = {
+                                "code": error.code,
+                                "message": error.message
+                            }
+                            res.json(errorResp)
+                            return
+                        }
+                        res.json(results)
+
+                        var transporter = nodemailer.createTransport({
+                            service: 'gmail',
+                            auth: {
+                                    type: 'OAuth2',
+                                    user: 'sanchit13103509cse@gmail.com',
+                                    clientId: '298351554194-lkm6ls22gg5seqhtq9u3htltt5aaqhp6.apps.googleusercontent.com',
+                                    clientSecret: 'zKrPXnFbUFfg0guacTpp2Bop',
+                                    refreshToken: '1/Wf3afmRUXsUk7zbRXNaUAln_m_9c7hn6uq4-VmTpOHZ_GjbUoK9sxj5hEipw2HG0'
+                            }
+                        })
+                        
+                        const url = `http://localhost:3007/welcome?token=${token}`
+
+                        var mailOptions = {
+                            from: 'Sanchit Mittal <sanchit13103509cse@gmail.com>',
+                            to: email,
+                            subject: 'Verify email',
+                            html: `Please click this email to confirm your email: <a href="${url}">${url}</a>`
+                        }
+                        
+                        transporter.sendMail(mailOptions, function (err: any, res: any) {
+                            if(err){
+                                console.log('Error');
+                            } else {
+                                console.log("Message sent: " + res.message);
+                            }
+                            transporter.close();
+                        })                       
+                        
+                    })
+                })
+                
             })
-        }())        
+        }())
     })
 export default auth;
+
+// .get((req: Request, res: Response) => {
+//     (async function fetchUsers(){
+//         dbConnection.query('SELECT * from students', function (error: MysqlError | null, results?: any, fields?: FieldInfo[]) {
+//             if (error) throw error;
+//             console.log(results)
+//             res.send(results)
+//         })
+//     }())
+//     // res.json(req.body);
+// })
